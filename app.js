841e0875a42d84e0af34d06a126404ede04bceba
Saskia H <sh224wg@student.lnu.se>
2024-10-16 00:41:04 +0200

    werkin express

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 
+const app = express()
+const port = 3000
 // Serve static files from the 'public' folder
+app.use(express.static('public'))
 
 // Parse URL coded data
 
