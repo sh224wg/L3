a681f361469f0a4a3e8c717bcb50ab4d237afcaf
Saskia H <sh224wg@student.lnu.se>
2024-10-24 17:34:35 +0200

    class cli

--- src/controlLineSetup.js
+++ src/controlLineSetup.js
@@ -4,26 +4,40 @@ import fs from 'fs'
 import path from 'path'
 import os from 'os'
 
-class ScraperCLI{
+class ScraperCLI {
+    constructor() {
+        this.scraper = new WebScraper()
+        this.url = process.argv[2]
+        this.shouldSaveToFile = process.argv.includes('--save')
+        this.desktopPath = path.join(os.homedir(), 'Desktop')
+        this.fileName = `scraped-content-${Date.now()}.json`
+        this.filePath = path.join(this.desktopPath, this.fileName)
+    }
+
+    /* const scraper = new WebScraper()
+    const url = process.argv[2] // url from command line
     
+    if(!url) {
+        console.log('Please enter a URL to scrape.')
+    }
+    
+    // save file to user desktop
+    const desktopPath = path.join(os.homedir(),'Desktop') // user desktop
+    const fileName = `scraped-content-${Date.now()}.json`// name for each file
+    const filePath = path.join(desktopPath, fileName)
+     */
+    //scraper
+    async run() {
+        try {
+            
+        }
+    }
+
 }
 
-const scraper = new WebScraper()
-const url = process.argv[2] // url from command line
-
-if(!url) {
-    console.log('Please enter a URL to scrape.')
-}
-
-// save file to user desktop
-const desktopPath = path.join(os.homedir(),'Desktop') // user desktop
-const fileName = `scraped-content-${Date.now()}.json`// name for each file
-const filePath = path.join(desktopPath, fileName)
-
-//scraper
-( async () => {
+=> {
     try {
-        if(!scraper.validateUrl(url)) {
+        if (!scraper.validateUrl(url)) {
             console.log('Invalid URL')
             process.exit(1)
         }
@@ -31,7 +45,7 @@ const filePath = path.join(desktopPath, fileName)
         const result = await scraper.scrapeWebPage(url)
         const jsonContent = JSON.stringify(result, null, 2)
 
-       // console.log('Scraped data:', jsonContent);
+        // console.log('Scraped data:', jsonContent);
 
         fs.writeFileSync(filePath, jsonContent)
         console.log(`Scraped data saved to ${filePath}`)
