d68a435e8d1e8ec1c163cb665147f44ce58b5d7b
Saskia H <sh224wg@student.lnu.se>
2024-10-24 14:08:54 +0200

    set up jest for tests

--- scraper.test.js
+++ scraper.test.js
@@ -2,4 +2,21 @@ import WebScraper from './src/module/scraper.js'
 import fetch from 'node-fetch'
 import { JSDOM } from 'jsdom'
 
-jest.mock('node-fetch')
+jest.mock('node-fetch', () => {
+    return {
+        __esModule: true,
+        default: jest.fn(),
+    }
+})
+
+describe('WebScraper', () => {
+    let scraper
+
+    beforeEach(() => {
+        scraper = new WebScraper()
+    })
+
+    test('validateUrl should return true for valid URLs', () => {
+        expect(scraper.validateUrl('https://example.com')).toBe(true)
+    })
+})
