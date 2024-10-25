cced83c11b9bdc4219bd7e9a6de344b0fd121880
Saskia H <sh224wg@student.lnu.se>
2024-10-25 13:00:16 +0200

    jsdoc

--- src/controlLineSetup.js
+++ src/controlLineSetup.js
@@ -5,9 +5,13 @@ import os from 'os'
 
 
 /**
- * 
+ * Class representing a command-line interface for the WebScraper.
  */
 class ScraperCLI {
+    /**
+     * Creates an instance of ScraperCLI.
+     * Initializes the WebScraper instance and sets up file paths for saving scraped content.
+     */
     constructor() {
         this.scraper = new WebScraper()
         this.url = process.argv[2]
@@ -16,7 +20,11 @@ class ScraperCLI {
         this.filePath = path.join(this.desktopPath, this.fileName)
     }
 
-    //scraper
+    /**
+     * Runs the web scraping process.
+     * Validates the input URL, scrapes the web page, retrieves the scraped data, and saves it to a file.
+     * Exits the process with an error message if any step fails.
+     */
     async run() {
         try {
             this.validateInput()
@@ -30,7 +38,8 @@ class ScraperCLI {
     }
 
     /**
-     * 
+     * Validates the input URL provided to the CLI.
+     * Exits the process with an error message if the URL is missing or invalid.
      */
     validateInput() {
         if (!this.url) {
@@ -44,16 +53,17 @@ class ScraperCLI {
     }
 
     /**
-     * 
-     * @param {*} result 
+     * Formats the scraped result into a readable string.
+     * @param {Object} result - The scraped result to be formatted.
+     * @returns {string} The formatted result as a string.
      */
     formatResult(result) {
         let content = 'Scraped Data:\n\n'
         for (const [key, value] of Object.entries(result)) { // iterate over object key value pairs
             content += `${key.toUpperCase()}:\n` // key to upper case
-            if(Array.isArray(value)) { // if value is array join elements into string otherwise add to content
-                content += value.map(item => typeof item === 'object' ? JSON.stringify(item, null, 2) : item).join('\n') + '\n' 
-            } else if(typeof value === 'object') {
+            if (Array.isArray(value)) { // if value is array join elements into string otherwise add to content
+                content += value.map(item => typeof item === 'object' ? JSON.stringify(item, null, 2) : item).join('\n') + '\n'
+            } else if (typeof value === 'object') {
                 content += JSON.stringify(value, null, 2) + '\n'
             } else {
                 content += value + '\n'
@@ -64,7 +74,8 @@ class ScraperCLI {
     }
 
     /**
-     * 
+     * Saves the scraped result to a file on the desktop.
+     * @param {Object} result - The scraped result to be saved.
      */
     saveToFile(result) {
         try {
